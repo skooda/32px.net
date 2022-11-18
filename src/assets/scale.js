cb7ed2b3b4133f01bb12c7e0283619238132ce1b
Pavel Škoda <p@v3l.cz>
2022-11-18 17:40:14 +0100

    Fix autozoom persistency

--- src/assets/scale.js
+++ src/assets/scale.js
@@ -5,11 +5,13 @@ let zoom_auto = () => {
     let s = Math.floor(Math.min(window.innerWidth, window.innerHeight)/34)
     main.style.zoom = s;
     sw.innerText = "Zoom: " + s + "x"
+    localStorage.setItem("zoom", s);
 }
 
 let zoom_reset = () => {
     main.style.zoom = 1
     sw.innerText = "Zoom: 1x"
+    localStorage.setItem("zoom", 1);
 }
 
 let zoom_toggle = () => {
@@ -22,4 +24,8 @@ let zoom_toggle = () => {
 
 // Init
 sw.addEventListener('click', zoom_toggle)
-zoom_auto()
+if (localStorage.getItem("zoom") == 1) {
+    zoom_reset()
+} else {
+    zoom_auto()
+}
